3811ca7e95ecaa02a2ef34b8f8510aae4adf7746
Majiro <majiro1996@gmail.com>
2023-04-06 23:43:59 +0200

    basic mapfile errors handled

--- main.c
+++ main.c
@@ -6,14 +6,14 @@
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 16:33:15 by manujime          #+#    #+#             */
-/*   Updated: 2023/04/06 19:04:31 by manujime         ###   ########.fr       */
+/*   Updated: 2023/04/06 23:42:41 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
 /*checks if the map extension is correct*/
-int	ft_extension_check(char *str)
+int	ft_extension_check(char *str, char *ex)
 {
 	int		len;
 	char	*aux;
@@ -22,24 +22,42 @@ int	ft_extension_check(char *str)
 	if (len < 4)
 		return (0);
 	aux = str + (len - 4);
-	if (ft_strcmp(aux, ".ber") == 0)
+	if (ft_strcmp(aux, ex) == 0)
 		return (1);
 	else
 		return (0);
 }
 
+void	ft_so_long(char *name)
+{
+	//code
+}
+
 int	main(int argc, char **argv)
 {
+	int	fd;
+
 	if (argc != 2)
 	{
 		if (argc > 2)
 			ft_printf("Error, too many arguments\n");
-		else
+		if (argc < 2)
 			ft_printf("Error, no mapfile found\n");
-		exit(1);
+		exit (1);
 	}
-	if (ft_extension_check(argv[1]))
+	if (ft_extension_check(argv[1], ".ber"))
 	{
-		ft_printf("this working mate\n");
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+		{
+			ft_printf("Error, no mapfile found\n");
+			exit (1);
+		}
+		close(fd);
+		ft_so_long(argv[1]);
+		return (0);
 	}
+	else
+		ft_printf("Error, not a .ber file\n");
+	exit (1);
 }
