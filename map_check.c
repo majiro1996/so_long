3f8d29fd14480ca95893d01e565d20d6be003a52
Manuel Jimenez Rodriguez <manujime@c3r3s2.42malaga.com>
2023-04-11 18:54:59 +0200

    walls in progres

--- map_check.c
+++ map_check.c
@@ -6,16 +6,11 @@
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 23:30:08 by manujime          #+#    #+#             */
-/*   Updated: 2023/04/11 10:28:02 by manujime         ###   ########.fr       */
+/*   Updated: 2023/04/11 17:44:35 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-void	ft_flood_fill(void)
-{
-	//code
-}
 
 /*checks that all rows are the same length 
 and that x and y size is bigger than 3*/
@@ -68,6 +63,53 @@ int	ft_elements(char **map)
 	return (1);
 }
 
+int	ft_el_count(char **map, char el)
+{
+	int		el_count;
+	int		c;
+	int		k;
+
+	el_count = 0;
+	c = 0;
+	while (map[c])
+	{
+		k = 0;
+		while (map[c][k])
+		{
+			if (map[c][k] == el)
+			{
+				el_count++;
+			}
+			k++;
+		}
+		c++;
+	}
+	return (el_count);
+}
+
+int	ft_walls(char **map, char *mapfile)
+{
+	int		c;
+	int		k;
+
+	c = 0;
+	while (map[c])
+	{
+		k = 0;
+		while (map[c][k])
+		{
+			if (map[c][k] != '1')
+			{
+				ft_printf("Error, not surrounded by walls\n");
+				return (0);
+			}
+			k++;
+		}
+		c++;
+	}
+	return (1);
+}
+
 int	ft_map_check(char *mapfile)
 {
 	int		clear;
@@ -78,12 +120,15 @@ int	ft_map_check(char *mapfile)
 	ft_print_char_matrix(map);
 	clear += ft_shape(map, mapfile);
 	clear += ft_elements(map);
-	//clear += ft_walls(map);
-	if (clear == 2)
+	if (ft_el_count(map, 'P') == 1 && ft_el_count(map, 'C') >= 1
+		&& ft_el_count(map, 'E') == 1)
+		clear += 1;
+	clear += ft_walls(map, mapfile);
+	if (clear == 3)
 	{
 		ft_printf("^-^ working");
 		return (1);
 	}
 	else
-		return(0);
+		return (0);
 }
